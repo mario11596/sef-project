634c5c74c689c8af8d49cc65805bbca7a09b42d0
mario_negovetic <mario.negovetic@estudent.hr>
2022-02-10 22:58:56 +0100

    promjena varijabli i ocitavanje dva potenciometra

--- sef-project/main.c
+++ sef-project/main.c
@@ -22,8 +22,14 @@ unsigned char keypad[4][4] = {	{'1','4','7','*'},
 static char output[4];
 static int counter = 6;
 static int vrata = 0;
-const char *provjera_lozinke = "1111";
-const char *zakljucavanje = "####";
+static int senzor = 0;
+static int flagPot = 0;
+static int pot1;
+static int pot2;
+static char potChar1[16];
+static char potChar2[16];
+const char *password_check = "1111";
+const char *password_lock = "####";
 unsigned char colloc, rowloc;
 
 char keyfind(){
@@ -92,69 +98,91 @@ char keyfind(){
 }
 
 
-/*ISR(TIMER0_COMP_vect) {
-		while(counter > 0){
-			PORTC = 0xff;
-			_delay_ms(100);
-			PORTC = 0x00;
-			_delay_ms(100);
-			counter--;
-		} 	
-		TIMSK ^= _BV(OCIE0);
-		PORTC = 0xff;
-		counter = 3;
-		
-		if(counter > 0){
-			PORTC ^= _BV(0x00);
-			_delay_ms(100);
-			counter--;
-		}		
-}+*/
-/*
-ISR(COMP){
-	IF(FLAGPWM ){
-		PORT |= _BV(NEKI);
-	}
-}
-
-ISR(OVF)D{
-	I++;
-	
-	IF(I >= N){
-		FLAGPWM = 1;
-		I = 0;
-	}
-	TU MORA ICI NEKA FUNKCIJA KOJAÆE GA DRZATI U JEDINICA NA OVRATKU, UGASITI PORT
-	};
-*/
-
 void buzzerDetection(){
 	while(counter > 0){
 		PORTC ^= _BV(7);
 		_delay_ms(100);
 		counter--;
 	}	
-	//counter = 6;
 }
 
-void okreni_vrata() {
+void writeLCD(uint16_t adc) {
+	
+	char adcStr[16];
+	itoa(round(adc), adcStr, 10);
+	
+	if(flagPot == 0){
+		lcd_gotoxy(6,0);
+		lcd_clrscr();
+		lcd_puts(adcStr);
+	} else if(flagPot == 1){
+		lcd_clrscr();
+		lcd_gotoxy(6,0);
+		lcd_puts(potChar1);
+		lcd_gotoxy(6,1);
+		lcd_puts(adcStr);
+	}
+	
+	if(pot1 == 200 && pot2 == 200){
+		lcd_clrscr();
+		lcd_puts("Otvoren sef!");
+		
+		
+	}
+
+}
+
+
+void readPotentiometer(){
+	while(1){
+		
+		if((ADC <= 205 && ADC >= 195) && flagPot == 0){
+			ADMUX |= _BV(MUX0);
+			flagPot = 1;
+			pot1 = 200;
+			itoa(pot1, potChar1, 10);
+			} else if((ADC <= 205 && ADC >= 195) && flagPot == 1){
+			pot2 = 200;
+		}
+		ADCSRA |= _BV(ADSC);
+
+		while (!(ADCSRA & _BV(ADIF)));
+
+		writeLCD(ADC);
+
+		_delay_ms(150);
+		
+		if(pot1 == 200 && pot2 == 200){
+			break;
+		}
+	}
+}
+
+
+void turn_servo() {
 	if(vrata == 1){
 		PORTD ^= _BV(5);
-		OCR1A = 300;
+		OCR1A = 300; // position +90°
 	} else{
 		PORTD ^= _BV(5);
-		OCR1A = 65;
-	}
-	
+		OCR1A = 65; // position -90°
+		
+		pot1 = 0;
+		pot2 = 0;
+		lcd_clrscr();
+		lcd_puts("Sef zatvoren!");
+		
+	}	
 }
 	
-void provjera(){
-	if(!strncmp(output, zakljucavanje, 1)){
+	
+void checkPassword(){
+	if(!strncmp(output, password_lock, 4) && vrata == 1){
 		vrata = 0;
-		okreni_vrata();
+		turn_servo();
 	}
 	
-	if(strncmp(output, provjera_lozinke, 4)){
+	if(strncmp(output, password_check, 4)){
 		lcd_clrscr();
 		lcd_puts("Netocna lozinka!");
 		counter = 6;
@@ -165,27 +193,32 @@ void provjera(){
 		counter = 2;
 		buzzerDetection();
 		vrata = 1;
-		okreni_vrata();
+		turn_servo();
 		
+		_delay_ms(300);
+		senzor = 1;
+		readPotentiometer();		
 	}
 	
 	memset(output, 0, sizeof(output));
 }
 
+
 void keyPassword(){
 	uint8_t i;
 	
 	for(i = 0; i < 4; i++){
-		char lozinka = keyfind();
+		char password = keyfind();
 		
-		output[i] = lozinka;
+		output[i] = password;
 		lcd_gotoxy(i,0);
 		lcd_clrscr();
 		lcd_puts(output);
 	}
 	output[4] = '\0';
-	provjera();
+	checkPassword();
 }
+
 
 ISR(TIMER0_COMP_vect){
 	PORTA ^= _BV(6);
@@ -198,34 +231,43 @@ ISR(TIMER1_COMPA_vect){
 	}
 }
 
+
 void initMain(){
-	DDRA |= _BV(6); //kontrast
-	DDRB = 0xff; //tipke
+	DDRA |= _BV(6); //contrast
+	DDRB = 0xff; //keypad
 	PORTB = 0x00;
 	
 	DDRC |= _BV(7); //buzzer
 	PORTC = _BV(7);
 	
-	DDRD |= _BV(5); //servo
+	DDRD |= _BV(5); //servo motor
 	TCNT1 = 0;
 	ICR1 = 2499;
 	
 	
-	TCCR1A = _BV(WGM11) | _BV(COM1A1);  //servo
+	TCCR1A = _BV(WGM11) | _BV(COM1A1); 
 	TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS10) | _BV(CS11);
-	OCR1A = 65; // poèetna pozicija: -90 °
+	OCR1A = 65; // position -90°
 	
 	
-	TCCR0 = _BV(WGM01) | _BV(WGM00) | _BV(COM01) | _BV(CS00); //LCD
+	TCCR0 = _BV(WGM01) | _BV(WGM00) | _BV(COM01) | _BV(CS00); 
 	OCR0 = 200;
 	TIMSK = _BV(OCIE0);
-	
+
 	sei();
 	
-
+	ADMUX = _BV(REFS0);
+	ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1);
+	
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
+	
+	lcd_puts("Upisite lozinku");
+	
+	memset(potChar1, 0, sizeof(potChar1));
+	memset(potChar2, 0, sizeof(potChar2));
 }
+
 
 int main(void){
 	initMain();
