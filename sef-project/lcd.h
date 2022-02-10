0a43e401a8d44f82a8733ad0aed3fd902222da39
mario_negovetic <mario.negovetic@estudent.hr>
2022-02-10 17:01:37 +0100

    dodan servo i buzzer za zakljucavanje

--- sef-project/lcd.h
+++ sef-project/lcd.h
@@ -83,7 +83,7 @@
  *  ports by adapting the LCD_DATAx_PORT and LCD_DATAx_PIN definitions.
  *  
  */
-#define LCD_PORT         PORTB       /**< port for the LCD lines   */
+#define LCD_PORT         PORTD       /**< port for the LCD lines   */
 #define LCD_DATA0_PORT   LCD_PORT     /**< port for 4bit data bit 0 */
 #define LCD_DATA1_PORT   LCD_PORT     /**< port for 4bit data bit 1 */
 #define LCD_DATA2_PORT   LCD_PORT     /**< port for 4bit data bit 2 */
@@ -92,8 +92,8 @@
 #define LCD_DATA1_PIN    1            /**< pin for 4bit data bit 1  */
 #define LCD_DATA2_PIN    2            /**< pin for 4bit data bit 2  */
 #define LCD_DATA3_PIN    3            /**< pin for 4bit data bit 3  */
-#define LCD_RS_PORT      PORTD     /**< port for RS line         */
-#define LCD_RS_PIN       5          /**< pin  for RS line         */
+#define LCD_RS_PORT      PORTA     /**< port for RS line         */
+#define LCD_RS_PIN       7          /**< pin  for RS line         */
 #define LCD_RW_PORT      PORTD    /**< port for RW line         */
 #define LCD_RW_PIN       6            /**< pin  for RW line         */
 #define LCD_E_PORT       PORTD     /**< port for Enable line     */
--- sef-project/main.c
+++ sef-project/main.c
@@ -8,9 +8,11 @@
 
 #include "lcd.h"
 
-#define KEY_PRT 	PORTA
-#define KEY_DDR		DDRA
-#define KEY_PIN		PINA
+#define KEY_PRT		PORTB
+#define KEY_DDR		DDRB
+#define KEY_PIN		PINB
+
+
 
 unsigned char keypad[4][4] = {	{'1','4','7','*'},
 								{'2','5','8','0'},
@@ -18,8 +20,10 @@ unsigned char keypad[4][4] = {	{'1','4','7','*'},
 								{'A','B','C','D'}};
 	
 static char output[4];
-static int counter = 3;
+static int counter = 6;
+static int vrata = 0;
 const char *provjera_lozinke = "1111";
+const char *zakljucavanje = "####";
 unsigned char colloc, rowloc;
 
 char keyfind(){
@@ -43,7 +47,6 @@ char keyfind(){
 				colloc = (KEY_PIN & 0x0F);
 			}while(colloc == 0x0F);
 
-		
 			KEY_PRT = 0xEF;           
 			asm("NOP");
 			colloc = (KEY_PIN & 0x0F);
@@ -89,7 +92,7 @@ char keyfind(){
 }
 
 
-ISR(TIMER0_COMP_vect) {
+/*ISR(TIMER0_COMP_vect) {
 		while(counter > 0){
 			PORTC = 0xff;
 			_delay_ms(100);
@@ -99,27 +102,75 @@ ISR(TIMER0_COMP_vect) {
 		} 	
 		TIMSK ^= _BV(OCIE0);
 		PORTC = 0xff;
-		counter = 3;		
+		counter = 3;
+		
+		if(counter > 0){
+			PORTC ^= _BV(0x00);
+			_delay_ms(100);
+			counter--;
+		}		
+}+*/
+/*
+ISR(COMP){
+	IF(FLAGPWM ){
+		PORT |= _BV(NEKI);
+	}
 }
 
+ISR(OVF)D{
+	I++;
+	
+	IF(I >= N){
+		FLAGPWM = 1;
+		I = 0;
+	}
+	TU MORA ICI NEKA FUNKCIJA KOJAÆE GA DRZATI U JEDINICA NA OVRATKU, UGASITI PORT
+	};
+*/
 
 void buzzerDetection(){
-	TIMSK = _BV(OCIE0);	
+	while(counter > 0){
+		PORTC ^= _BV(7);
+		_delay_ms(100);
+		counter--;
+	}	
+	//counter = 6;
+}
+
+void okreni_vrata() {
+	if(vrata == 1){
+		PORTD ^= _BV(5);
+		OCR1A = 300;
+	} else{
+		PORTD ^= _BV(5);
+		OCR1A = 65;
+	}
+	
 }
 	
 void provjera(){
+	if(!strncmp(output, zakljucavanje, 1)){
+		vrata = 0;
+		okreni_vrata();
+	}
+	
 	if(strncmp(output, provjera_lozinke, 4)){
 		lcd_clrscr();
-		lcd_puts("Netocna lozinka!");	
+		lcd_puts("Netocna lozinka!");
+		counter = 6;
 		buzzerDetection();	
 	} else  {
 		lcd_clrscr();
 		lcd_puts("Tocna lozinka!");
+		counter = 2;
+		buzzerDetection();
+		vrata = 1;
+		okreni_vrata();
+		
 	}
+	
 	memset(output, 0, sizeof(output));
 }
-
-
 
 void keyPassword(){
 	uint8_t i;
@@ -136,21 +187,41 @@ void keyPassword(){
 	provjera();
 }
 
+ISR(TIMER0_COMP_vect){
+	PORTA ^= _BV(6);
+}
+
+
+ISR(TIMER1_COMPA_vect){
+	if(vrata == 1){
+		PORTD ^= _BV(5);
+	}
+}
+
 void initMain(){
-	DDRD = _BV(4);
-	DDRA = 0xff;
-	PORTA = 0x00;
+	DDRA |= _BV(6); //kontrast
+	DDRB = 0xff; //tipke
+	PORTB = 0x00;
 	
-	DDRC = _BV(0); //buzzer
-	PORTC = _BV(0);
+	DDRC |= _BV(7); //buzzer
+	PORTC = _BV(7);
 	
-	TCCR1A = _BV(COM1B1) | _BV(WGM10);
-	TCCR1B = _BV(WGM12) | _BV(CS11);
-	OCR1B = 70;
+	DDRD |= _BV(5); //servo
+	TCNT1 = 0;
+	ICR1 = 2499;
 	
-	TCCR0 = _BV(WGM01) | _BV(CS02) | _BV(CS00);
-	OCR0 = 100;
+	
+	TCCR1A = _BV(WGM11) | _BV(COM1A1);  //servo
+	TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS10) | _BV(CS11);
+	OCR1A = 65; // poèetna pozicija: -90 °
+	
+	
+	TCCR0 = _BV(WGM01) | _BV(WGM00) | _BV(COM01) | _BV(CS00); //LCD
+	OCR0 = 200;
+	TIMSK = _BV(OCIE0);
+	
 	sei();
+	
 
 	lcd_init(LCD_DISP_ON);
 	lcd_clrscr();
@@ -160,6 +231,6 @@ int main(void){
 	initMain();
 	
 	while (1){
-		keyPassword();	
+		keyPassword();
 	}
 }
